d0459aa242996123c4a11bc3113f83bb5891d536
mustafa2smi-ui <mustafa2smi@gmail.com>
2026-09-04 21:34:01 +0530

    Add Islamic content databases for stories and quotes

---
+++ Islamic-content-data.js
@@ -0,0 +1,38 @@
+// 1. Sabak Amooz Waqiat Database
+const waqiatDatabase = [
+  {
+    id: 1,
+    date: "04 Sep 2026",
+    title: "Maa Ki Dua Aur Hazrat Bayazid Bastami (R.A)",
+    snippet: "Sardiyon ki ek thandi raat me Hazrat Bayazid Bastami ki walida ne paani maanga. Matke me paani khatam tha, wo kooen se paani lekar aaye to walida so chuki theen...",
+    fullStory: "Sardiyon ki ek sakht thandi raat me Hazrat Bayazid Bastami (R.A) ki walida mohterma ne paani maanga. Ghar ke matke me paani khatam tha. Aap door kooen par gaye aur paani lekar wapas aaye to walida sahiba dobara neend me chali gayi theen.\n\nAap kooze me paani liye raat bhar walida ke sarhane khade rahe ke kahin aawaz dene se unki neend me khalal na pade. Subah jab walida ki aankh khuli to unhone apne bete ko paani ka pyala haath me liye sardi se kaanpte hue dekha. Walida ke dil se aisi aah o dua nikli jisne Bayazid ko 'Sultan-ul-Arifeen' bana diya.\n\nSabaq: Walidain ki khidmat aur unka adab insaan ko aam se khas bana deta hai.",
+    category: "Walidain Ka Adab"
+  }
+];
+
+// 2. Aaj Ki Acchi Baat (WhatsApp Styled Quotes)
+const acchiBaatDatabase = [
+  {
+    id: 1,
+    date: "04 Sep 2026",
+    hijriDate: "22 Rabi al-Awwal 1448",
+    title: "Khamoshi Ka Wazan",
+    quoteLines: [
+      "Awaaz Kaan Tak Aati Hai, ...",
+      "Magar Khamoshi Aasmaan Tak Jaati Hai ...",
+      "Sabr wo sawari hai jo apne sawaar ko girne nahi deti."
+    ],
+    closingDua: "Allah Tala hum sab ko narm guftagu aur sabr ki toufeeq ata farmaye."
+  }
+];
+
+// 3. Hadees Sharif Database
+const hadeesDatabase = [
+  {
+    id: 1,
+    arabic: "خَيْرُكُمْ مَنْ تَعَلَّمَ الْقُرْآنَ وَعَلَّمَهُ",
+    tarjuma: "Tum me se sabse behtar shakhs wo hai jo Quran seekhe aur doosron ko sikhaye.",
+    reference: "Sahih Bukhari: 5027",
+    wazahat: "Is Hadees me Quran-e-Pak ke parhne, samajhne aur aage sikhane wale ko ummat ka sabse afzal tareen shakhs qarar diya gaya hai."
+  }
+];
